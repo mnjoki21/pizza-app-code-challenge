f01d278b175aeb37d4be619cf592098a657ed026
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-01 00:34:11 +0300

    adding routes controllers in restaurants

--- app/controllers/restuarants_controller.rb
+++ app/controllers/restuarants_controller.rb
@@ -4,4 +4,22 @@ class RestuarantsController < ApplicationController
         restuarants = Restuarant.all
         render json: restuarants 
     end
+
+    def show
+        restuarants = Restuarant.find_by(params[:id])
+        render json: restuarants
+    end
+
+
+    def destroy  
+        restuarant = Restuarant.find(params[:id])
+        restuarant.destroy
+        head :no_content
+    end
+
+    private
+
+    def not_found_response 
+        render json: {error: "Restaurant not found"}, status: :not_found
+    end
 end
