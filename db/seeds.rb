1509e6e14c438b8236a2e8fdd23d8d04ea227eba
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-30 23:47:53 +0300

    adding some data to seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "seeding restuarants"
+Restaurant.create!([
+  {
+    name: "mama oliech",
+    address: "kilimani"
+  },
+  {
+    name: "brew bristo",
+    address: "westlands"
+  },
+  {
+    name: "pizza inn",
+    address: "ngong road"
+  },
+   {
+    name: "dominos",
+    address: "marsabit plaza"
+  }
+])
+
+puts "🍕 Seeding pizzas..."
+Pizza.create!([
+  {
+    name: "Cheese",
+    ingredients: "Dough, Tomato Sauce, Cheese"
+  },
+  {
+    name: "Pepperoni",
+    ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni"
+  },
+  {
+    name: "California",
+    ingredients: "Dough, Sauce, Ricotta, Red peppers, Mustard"
+  },
+  {
+    name: "Greek",
+    ingredients: "Dough, Sauce, Cheese, Feta, Artichokes, Kalamata Olives"
+  },
+  {
+    name: "Margherita",
+    ingredients: "Dough, Sauce, Cheese, Basil"
+  },
+  {
+    name: "BBQ Chicken",
+    ingredients: "Dough, BBQ sauce, Chicken, Cilantro, Red Onions, Fontina Cheese"
+  },
+  {
+    name: "Hawaiian",
+    ingredients: "Dough, Sauce, Cheese, Ham, Pineapple "
+  },
+  {
+    name: "Meat Lovers",
+    ingredients: "Dough, Sauce, Cheese, Pepperoni, Sausage, Meatballs, Mushrooms"
+  },
+  {
+    name: "Frutti di mare",
+    ingredients: "Dough, Sauce, Cheese, Tuna, Shrimp, Mussels"
+  },
+  {
+    name: "Veggie",
+    ingredients: "Dough, Sauce, Cheese, Red Peppers, Green Peppers, Onions, Mushrooms"
+  }
+])
