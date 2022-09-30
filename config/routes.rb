d3885dc34ecc4744241536ec11d29129cda7142b
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-30 23:55:51 +0300

    adding resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :restuarants, only: [:index, :show, :destroy]
+  resources :restuarant_pizzas , only: :create
+  resources :pizza , only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
