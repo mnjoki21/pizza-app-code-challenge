3d3867f81168c8264089a0c8bf2c6cc180b518d3
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-02 22:23:29 +0300

    debugging

--- app/models/restuarant.rb
+++ app/models/restuarant.rb
@@ -1,4 +1,5 @@
 class Restuarant < ApplicationRecord
-    has_many :pizzas , through: :restuarant_pizzas
+    
     has_many :restuarant_pizzas
+    has_many :pizzas , through: :restuarant_pizzas
 end
