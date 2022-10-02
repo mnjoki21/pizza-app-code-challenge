bdc5ed6781608e3d8eeae92baad3c1281aaae3d1
mnjoki21 <mitchellenjoki7@gmail.com>
2022-10-02 22:30:10 +0300

    adding create method for reataurant pizza

--- app/controllers/restuarant_pizzas_controller.rb
+++ app/controllers/restuarant_pizzas_controller.rb
@@ -3,6 +3,17 @@ class RestuarantPizzasController < ApplicationController
     def show
         restuarant_pizza = RestuarantPizza.all
         render json: restuarant
+    end
+
+    def create
+        restuarant_pizza = RestuarantPizza.create(restuarant_pizza_params)
+        render json: restuarant_pizza, status: :created
+    end
+
+    private
+
+    def restuarant_pizza_params
+        params.permit(:pizza, :restuarant, :price)
         
     end
 end
